7579c7d1693efa1f18403482b3b2ff1c9e6a91ef
John Doe <iam@alexxnb.ru>
2019-04-24 11:47:33 +0300

    made ttf generation

--- staff/scripts/buildfonts.js
+++ staff/scripts/buildfonts.js
@@ -3,19 +3,31 @@ const sh = require("shelljs");
 const path = require("path");
 const {root} = require("./_helpers.js");
 const glyphr = require("./glyphr");
+const svg2ttf = require('svg2ttf');
 
-sh.rm('-rf',path.join(root,'__fonts'));
+const fontsdir = path.join(root,'__fonts');
 
+sh.rm('-rf',fontsdir);
+sh.mkdir('-p',fontsdir);
 
-const fontfiles = fs.readdirSync(path.join(root,'src')).filter(item => item.endsWith('.txt'));
-
-sh.mkdir('-p',path.join(root,'__fonts'));
-fontfiles.map(file => {
+// Generating SVG fonts
+const projects = fs.readdirSync(path.join(root,'src')).filter(item => item.endsWith('.txt'));
+projects.map(file => {
     let project_file = path.join(root,'src',file);
-    let output_file = path.join(root,'__fonts',file.replace('.txt','.svg'));
+    let output_file = path.join(fontsdir,file.replace('.txt','.svg'));
 
-    console.log(`Building ${file}...`);
+    console.log(`Building SVG font from ${file}...`);
 
     glyphr.export_svgfont(project_file,output_file);
 });
 
+// Generating TTF fonts from SVG fonts
+const svgfiles = fs.readdirSync(fontsdir).filter(item => item.endsWith('.svg'));
+svgfiles.map(file => {
+    let svg_file = path.join(fontsdir,file);
+    let ttf_file = path.join(fontsdir,file.replace('.svg','.ttf'));
+
+    console.log(`Building TTF font from ${file}...`);
+    let ttf = svg2ttf(fs.readFileSync(svg_file, 'utf8'), {});
+    fs.writeFileSync(ttf_file, Buffer.from(ttf.buffer));
+});
